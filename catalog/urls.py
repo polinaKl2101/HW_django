c3ae4373488862f4cc26e21f49870059caa4bcd8
polinaKl2101 <pklopunova@gmail.com>
2023-06-05 05:21:43 +0600

    HW3 add includes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import homepage, contacts, current_prod
+from catalog.views import HomepageListView, contacts, Current_prodDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', homepage, name='homepage'),
+    path('', HomepageListView.as_view(), name='homepage'),
     path('contacts/', contacts, name='contacts'),
-    path('current_prod', current_prod, name='current_prod')
+    path('current_prod', Current_prodDetailView.as_view(), name='current_prod')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
 
 from catalog.models import Product
+from django.views import generic
 
 
-def homepage(request):
-    products_list = Product.objects.all()
-    context = {
-        'object_list': products_list,
+class HomepageListView(generic.ListView):
+    model = Product
+    extra_context = {
         'title': 'Магазин'
     }
-    return render(request, 'catalog/product.html', context=context)
 
+
+class Current_prodDetailView(generic.DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = context_data['object']
+        return context_data
+
+
+# def current_prod(request):
+#     products_list = Product.objects.all()
+#     context = {
+#         'object_list': products_list
+#     }
+#     return render(request, 'catalog/homepage.html', context)
+
+
+# class ContactsListView(generic.ListView):
+#     model = Product
+#      extra_context = {
+#          'title': 'Контакты'
+#      }
 
 def contacts(request):
     extra_context = {
@@ -19,9 +41,4 @@ def contacts(request):
     return render(request, 'catalog/contact.html')
 
 
-def current_prod(request):
-    products_list = Product.objects.all()
-    context = {
-        'object_list': products_list
-    }
-    return render(request, 'catalog/homepage.html', context)
+
