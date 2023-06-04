43e418ac5f3199a3da9d9e7e1af0903b63db8cdd
polinaKl2101 <pklopunova@gmail.com>
2023-06-05 03:22:02 +0600

    HW2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=150, verbose_name='Название категории')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f"{self.category_name}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('pk',)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    last_change_date = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f"Наименование: {self.product_name}. " \
+               f"Категория: {self.category}. " \
+               f"Цена за покупку: {self.price}. " \
+               f"Дата создания: {self.date_created}"
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('pk',)
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def homepage(request):
-    return render(request, 'catalog/homepage.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'SkyStore'
+    }
+
+    return render(request, 'catalog/homepage.html', context=context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
-
